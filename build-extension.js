b1adf86d9f21a55313c9586f0f3ad46604078284
O3H <owen77stubbs@gmail.com>
2026-01-27 07:23:19 +0000

    fix zip outputting at top level instead of in an extension dir

--- build-extension.js
+++ build-extension.js
@@ -2,17 +2,17 @@ import fs from 'fs'
 import path from 'path'
 import archiver from 'archiver'
 
-const ZIP_NAME = 'earthmc-dynmap.zip'
+const EXT_NAME = 'earthmc-dynmap'
 
-const output = fs.createWriteStream(path.join('dist', ZIP_NAME))
+const output = fs.createWriteStream(path.join('dist', EXT_NAME+".zip"))
 const archive = archiver('zip', { zlib: { level: 9 } })
 archive.pipe(output)
 
-archive.directory('src', 'src')
-archive.file('manifest.json', { name: 'manifest.json' })
-archive.file('icon48.png', { name: 'icon48.png' })
-archive.file('icon128.png', { name: 'icon128.png' })
-archive.file('README.md', { name: 'README.md' })
-archive.file('style.css', { name: 'style.css' })
+archive.directory('src', EXT_NAME+'/src')
+archive.file('manifest.json', { name: EXT_NAME+'/manifest.json' })
+archive.file('icon48.png', { name: EXT_NAME+'/icon48.png' })
+archive.file('icon128.png', { name: EXT_NAME+'/icon128.png' })
+archive.file('README.md', { name: EXT_NAME+'/README.md' })
+archive.file('style.css', { name: EXT_NAME+'/style.css' })
 
 archive.finalize().then(() => console.log('Successfully compiled extension zip. Output at: dist/earthmc-dynmap.zip'))
