d28ae2cd6191df06a694ed18a1efe64a6fe8346c
O3H <owen77stubbs@gmail.com>
2026-03-10 00:14:04 +0000

    fix bug where archive mode would not work before 2023

--- src/main.js
+++ src/main.js
@@ -561,7 +561,7 @@ async function lookupPlayer(playerName, showOnlineStatus = true) {
 	const balance = player.stats.balance
 	const town = player.town.name
 	const nation = player.nation.name
-	const lastOnline = new Date(player.timestamps.lastOnline).toLocaleDateString('fr')
+	const lastOnline = new Date(player.timestamps.lastOnline).toLocaleDateString()
 	let onlineStatus = '<span id="player-lookup-online" style="color: {online-color}">{online}</span>'
 	const about = (!player.about || player.about == '/res set about [msg]') ? '' : `<br><i>${player.about}</i>`
 	let rank = 'Townless'
@@ -680,8 +680,8 @@ async function getArchive(data) {
 	
 	// markers.json URL changed over time
 	const markersURL = 
-		date < 20240701 ? "https://earthmc.net/map/aurora/standalone/MySQL_markers.php?marker=_markers_/marker_earth.json" :
 		date < 20230212 ? "https://earthmc.net/map/aurora/tiles/_markers_/marker_earth.json" :
+		date < 20240701 ? "https://earthmc.net/map/aurora/standalone/MySQL_markers.php?marker=_markers_/marker_earth.json" :
 		"https://map.earthmc.net/tiles/minecraft_overworld/markers.json" // latest
 
 	const archive = await fetchJSON(PROXY_URL + getArchiveURL(date, markersURL))
@@ -696,7 +696,8 @@ async function getArchive(data) {
 		actualArchiveDate = archive[0].timestamp
 	}
 
-	actualArchiveDate = new Date(parseInt(actualArchiveDate)).toLocaleDateString()
+	// THIS HAS TO BE EN-CA SO REPLACING DASHES WORKS TO MATCH STORED DATE
+	actualArchiveDate = new Date(parseInt(actualArchiveDate)).toLocaleDateString('en-ca')
 	document.querySelector('#current-map-mode-label').textContent += ` (${actualArchiveDate})`
 	loadingMessage.remove()
 	if (actualArchiveDate.replaceAll('-', '') != date) {
